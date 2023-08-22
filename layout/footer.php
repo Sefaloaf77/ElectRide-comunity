06062b51633c37db2d309aa7320c174f87e24313
Sefalo AF <sefaloaf@gmail.com>
2023-08-22 07:09:13 +0700

    final project

---
+++ layout/footer.php
@@ -0,0 +1,82 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+    <div class="footer-content position-relative">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-info">
+                        <h3>ElektRide <br>Community</h3>
+                        <p>
+                            Malang, Jawa Timur, Indonesia<br><br>
+                            <strong>Phone:</strong> +62 877 6113 9183<br>
+                            <strong>Email:</strong> sefalo@gmail.com<br>
+                        </p>
+                        <div class="social-links d-flex mt-3">
+                            <a href="#" class="d-flex align-items-center justify-content-center"><i class="fa-brands fa-facebook"></i></a>
+                            <a href="#" class="d-flex align-items-center justify-content-center"><i class="fa-brands fa-instagram"></i></a>
+                            <a href="#" class="d-flex align-items-center justify-content-center"><i class="fa-brands fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div><!-- End footer info column-->
+
+                <div class="col-lg-2 col-md-3 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.html">HOME</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="profile.html">PROFILE</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="visi-misi.html">VISI-MISI</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="produk.html">PRODUK KAMI</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="kontak.html">KONTAK KAMI</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="tentang.html">TENTANG KAMI</a>
+                        </li>
+                    </ul>
+                </div><!-- End footer links column-->
+
+                <div class="col-lg-2 col-md-3 footer-links">
+                    <h4>Sidebar</h4>
+                    <ul>
+                        <li class="nav-item">
+                            <a class="nav-link active"href="artikel.html">ARTIKEL</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="event.html">EVENT</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="galeri.html">GALERI FOTO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="klien.html">KLIEN KAMI</a>
+                        </li>
+                    </ul>
+                </div><!-- End footer links column-->
+
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-legal text-center position-relative">
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span>Sertikom BNSP JWD</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                Designed by <a href="https://www.linkedin.com/in/sefaloaf/"> Sefalo 'Aadila Faruq</a>
+            </div>
+        </div>
+    </div>
+
+</footer>
+<!-- End Footer -->
